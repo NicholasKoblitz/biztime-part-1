315c6409cc43d5fb48cff28a426397be39d60be4
Nick <nick@Sabrinas-MacBook-Pro-4.local>
2022-05-10 19:08:35 -0400

    Finished invoice routes

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -16,11 +16,24 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
     try {
-        if(req.params.id) {
+        const ids = await db.query(`SELECT id FROM invoices`);
+        let validId;
+        for(let i of ids.rows) {
+            if(parseInt(req.params.id) === i.id){
+                validId = i.id
+            }
+        }
+
+        if(validId) {
             const results = await db.query(`
             SELECT * FROM invoices
             WHERE id=$1`,
-            [req.params.id])
+            [validId])
+            
+            return res.json({invoice: results.rows[0]});
+        }
+        else {
+            return res.json({msg: "Id not found"})
         }
     }
     catch(e) {
@@ -31,7 +44,14 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
     try {
+        const { comp_code, amt } = req.body;
+        const results = await db.query(`
+            INSERT INTO invoices (comp_code, amt)
+            VALUES ($1, $2)
+            RETURNING id, comp_code, amt, paid, add_date, paid_date`, 
+            [comp_code, amt]);
 
+            return res.json({invoice: results.rows[0]})
     }
     catch(e) {
         next(e);
@@ -39,9 +59,28 @@ router.post("/", async (req, res, next) => {
 })
 
 
-router.put("/", async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
     try {
+        const ids = await db.query(`SELECT id FROM invoices`);
+        let validId;
+        for(let i of ids.rows) {
+            if(parseInt(req.params.id) === i.id){
+                validId = i.id
+            }
+        }
+        if(validId) {
+            const { amt } = req.body;
+            const results = await db.query(`
+            UPDATE invoices SET amt=$1
+            WHERE id=$2
+            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+            [amt, req.params.id])
 
+            return res.json({invoice: results.rows[0]})
+        }
+        else {
+            return res.json({msg: "Id not found"})
+        }
     }
     catch(e) {
         next(e);
@@ -49,9 +88,26 @@ router.put("/", async (req, res, next) => {
 })
 
 
-router.delete("/", async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
     try {
+        const ids = await db.query(`SELECT id FROM invoices`);
+        let validId;
+        for(let i of ids.rows) {
+            if(parseInt(req.params.id) === i.id){
+                validId = i.id
+            }
+        }
+        if(validId) {
+            const results = db.query(`
+            DELETE FROM invoices 
+            WHERE id=$1`,
+            [validId]);
 
+            return res.json({msg: "Deleted"})
+        }
+        else {
+            return res.json({msg: "Id not found"})
+        }
     }
     catch(e) {
         next(e);
