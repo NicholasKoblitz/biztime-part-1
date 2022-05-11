bb2474f570a718fd4a6d665887be99333b4830ad
NicholasK <nicholask1@outlook.com>
2022-05-10 21:33:21 -0400

    Updated DB_URI

--- src/db.js
+++ src/db.js
@@ -3,10 +3,11 @@ const { Client } = require("pg");
 let DB_URI;
 
 if(process.env.NODE_ENV === "test") {
-    DB_URI = "postgresql:///biztime_test";
+    DB_URI = "postgresql://nick:Sonic0125@localhost:5432/biztime_test";
 }
 else {
-    DB_URI = "postgresql:///biztime";
+    // DB_URI = "postgresql:///biztime";
+    DB_URI = "postgres://nick:Sonic0125@localhost:5432/biztime"
 }
 
 let db = new Client({
